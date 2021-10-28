7889941e4dea97fefd40a2a7798861b6a93cd28d
Kartik <ksihotiya@gmail.com>
2021-10-28 10:27:38 +0530

    added axios

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import './App.css';
+import axios from "axios"
 const key = process.env.REACT_APP_KEY;
 const base = process.env.REACT_APP_BASE;
 
@@ -8,16 +9,15 @@ const base = process.env.REACT_APP_BASE;
 function App() {
   const [query , setQuery] = useState('');
   const [weather , setWeather] = useState({});
-  const search = evt => {
+  const [error,setError] = useState('');
+  
+  const search = async evt => {
     if (evt.key === 'Enter')
     {
-      fetch(`${base}weather?q=${query}&units=metric&APPID=${key}`)
-        .then(res => res.json())
-        .then(result => {
-          setWeather(result);
-          setQuery('');
-          console.log(result);
-        });
+      await axios.get(`${base}weather?q=${query}&units=metric&APPID=${key}`
+      ).then((res) => {setWeather(res.data);setQuery('');} 
+      ).catch((err) => {setWeather({});setQuery('');setError("location not found")}); 
+      
     }
 
   }
@@ -48,14 +48,14 @@ function App() {
       <div className="celsius">C</div>
     </div>
     <div className="weather">
-      <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} />
+      <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt="" />
       <p>{weather.weather[0].main} {weather.weather[0].icon[2] === 'n' ? (<>Night</>):(<>Day</>) }</p>
     </div>
     
     </div>
     
     </>):(<div className="error">
-    {weather.message}
+    {error}
     </div>)}
 
     </>
